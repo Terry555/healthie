5f4c3fd2fe4f1e4758bd0a46c0ae133a2609f795
github username <github email address>
2019-06-10 23:50:50 -0400

    added controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  before_action :authorized
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :conversation_memberships
   has_many :conversations, :through => :conversation_memberships
   belongs_to :user_group, optional: true
+
+
+  def index
+    @users = User.all
+  end
+
 end
